84765211b6dc19bc0313dc7c2b4b7a830cab4472
Endaxe <94972517+Endaxe@users.noreply.github.com>
2023-05-09 15:04:53 +0200

    Mousemotionlistener + x y kord

--- src/Slutprojekt.java
+++ src/Slutprojekt.java
@@ -3,6 +3,9 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -15,8 +18,11 @@ public class Slutprojekt extends Canvas implements Runnable {
     private Thread thread;
     private boolean running = false;
     private int frames = 0;
+    Label l;
 
     private BufferedImage bar;
+
+    private int mouseX;
 
 
     public Slutprojekt() {
@@ -38,6 +44,8 @@ public class Slutprojekt extends Canvas implements Runnable {
         JFrame frame = new JFrame();
         frame.add(this);
         //frame.addKeyListener(new MyKeyListener());
+        frame.addMouseMotionListener(new MyMouseMotionListener());
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
@@ -73,7 +81,10 @@ public class Slutprojekt extends Canvas implements Runnable {
     public static void main(String[] args) {
         Slutprojekt Mittslutprojekt = new Slutprojekt();
         Mittslutprojekt.start();
+
     }
+
+
 
     public synchronized void start() {
         running = true;
@@ -106,8 +117,68 @@ public class Slutprojekt extends Canvas implements Runnable {
                 render();
                 // delta--;
             }
+
+        }
+    }
+
+        public class MyMouseMotionListener extends Frame implements MouseMotionListener {
+            MyMouseMotionListener(){
+                    l=new Label();
+                    l.setBounds(600,40,100,20);
+                    add(l);
+
+            }
+
+            @Override
+            public void mouseDragged(MouseEvent e) {
+
+                l.setText("X=" + e.getX() + ", Y=" + e.getY());
+
+                if (bar.getWidth() > 300){
+                    Graphics g = getGraphics();
+                    g.setColor(Color.BLACK);
+                    g.fillOval(e.getX(), e.getY(), 10, 10);
+                }
+                else{
+                    Graphics g = getGraphics();
+                    g.setColor(Color.BLACK);
+                    g.fillOval(e.getX(), e.getY(), 20, 20);
+                }
+            }
+
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                l.setText("X="+e.getX()+", Y="+e.getY());
+            }
+        }
+
+        public class MyMouseListener implements MouseListener {
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int mouseX = e.getX();
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+            }
         }
         //stop();
-    }
 }
+
+
+
 
